abaab2d9b548b24216609f51defa3ea6c6fead12
noobpiyush <83893659+noobpiyush@users.noreply.github.com>
2024-02-03 18:42:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(cors({
     credentials: true,
-    origin: "https://65be20c633787d7de1ef33bc--calm-tartufo-e6cc5d.netlify.app"
+    origin: "http://express-blogs-by-piyush.s3-website.ap-south-1.amazonaws.com"
 }))
 app.use(express.json());
 // const _dirname = path.dirname("");
